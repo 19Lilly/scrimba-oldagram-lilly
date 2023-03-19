3784f3866ece84e3f6058fa7f7b5ec5d9c170437
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-03-19 16:24:15 +0100

    like/unlike function

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
+        isLiked: false,
+        uuid: '337be4e5-45d9-430e-b661-e9526bb461ef'
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +17,9 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        isLiked: false,
+        uuid: '6bd98d66-7891-4e50-a522-66bd6de6569c'
     },
         {
         name: "Joseph Ducreux",
@@ -24,14 +28,43 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        isLiked: false,
+        uuid:'8f315418-e8f8-4d41-8619-9add6ebf4ca8'
     }
 ]
 
-let sectionEl = document.getElementById("section-el")
 
 
+document.addEventListener('click', function(e){
+    if(e.target.dataset.like){
+       handleLikeClick(e.target.dataset.like) 
+    }
+})
+
+function handleLikeClick(postId){ 
+    const targetOldagramPost = posts.filter(function(post){
+        return post.uuid === postId
+    })[0]
+
+    if (targetOldagramPost.isLiked){
+        targetOldagramPost.likes--
+    }
+    else{
+        targetOldagramPost.likes++ 
+    }
+    targetOldagramPost.isLiked = !targetOldagramPost.isLiked
+    console.log(targetOldagramPost.likes)
+    renderPost(posts)
+}
+
+
+
+
+function renderPost(posts){
+    let sectionEl = document.getElementById("section-el")
     for (let i = 0; i < posts.length; i++){
+       
 
        sectionEl.innerHTML += 
        `<div class="post">
@@ -46,7 +79,7 @@ let sectionEl = document.getElementById("section-el")
                     <img class="post-img" src="${posts[i].post}">
                     
                     <div class="social-media">
-                        <button id="click-me"><img class="icons" src="images/icon-heart.png"></button>
+                        <img data-like="${posts[i].uuid}"class="icons" src="images/icon-heart.png">
                         <img class="icons" src="images/icon-comment.png">
                         <img class="icons" src="images/icon-dm.png">
                     </div>
@@ -55,9 +88,11 @@ let sectionEl = document.getElementById("section-el")
                         <span class="username bold-text">${posts[i].username}</span>${posts[i].comment}  
                     </p>
         </div>`
-
       
 }
+}
+
+renderPost(posts)
 
 
 
